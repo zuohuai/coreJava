1dc00cb6826104390d09b1984ddb1dda4581962f
zuo_huai <zuo_huai@163.com>
2015-12-28 11:27:15 +0800

    修改不依赖框架

--- src/main/java/com/edu/mapEditor/MainMapEditor.java
+++ src/main/java/com/edu/mapEditor/MainMapEditor.java
@@ -1,24 +1,45 @@
 package com.edu.mapEditor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.my9yu.common.console.Console;
-
-public class MainMapEditor {
+public class MainMapEditor implements Runnable {
 	/** 默认的上下文配置名 */
 	private static final String DEFAULT_APPLICATION_CONTEXT = "applicationContext-mapEditor.xml";
 
-	public static void main(String[] arguments) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-		final ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(DEFAULT_APPLICATION_CONTEXT);
+	public static void main(String[] arguments) throws InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
+		final ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				DEFAULT_APPLICATION_CONTEXT);
 		try {
-			Console console = new Console(applicationContext);
-			console.start();
+			MainMapEditor mainMapEditor = new MainMapEditor();
+			Thread thread = new Thread(mainMapEditor);
+			thread.start();
 		} catch (Exception exception) {
 			exception.printStackTrace();
 			if (applicationContext.isRunning()) {
 				applicationContext.destroy();
 			}
 		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			String line;
+			try {
+				line = in.readLine();
+				System.out.println(line);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 }
