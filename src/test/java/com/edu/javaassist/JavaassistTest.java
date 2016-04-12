2c9fc5b2e4f912cc3bf28ecc6c9f4848066adcc9
zuo_huai <zuo_huai@163.com>
2016-04-12 12:04:17 +0800

    兼容1.8配置

--- src/test/java/com/edu/javaassist/JavaassistTest.java
+++ src/test/java/com/edu/javaassist/JavaassistTest.java
@@ -1,5 +1,48 @@
 package com.edu.javaassist;
 
+import org.junit.Test;
+
+import javassist.ClassClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
 public class JavaassistTest {
 
+	private ClassPool pool = ClassPool.getDefault();
+
+	{
+		// 用来解决在web容器中不能找到对应class的问题
+		pool.insertClassPath(new ClassClassPath(this.getClass()));
+	}
+
+	/**
+	 * 创建类和创建方法
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_create_class() throws Exception {
+
+		CtClass cc = pool.makeClass("com.edu.javaassist.HelloWorld");
+		CtMethod mthd = CtNewMethod.make("public Integer getInteger() { return null; }", cc);
+		cc.addMethod(mthd);
+		cc.writeFile();
+	}
+
+	/**
+	 * 测试javaassist 在AOP中的应用
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_aop() throws Exception {
+		// 对已有代码每次move执行时做埋点
+		CtClass cc = pool.get("com.edu.javaassist.Point");
+		CtMethod m = cc.getDeclaredMethod("move");
+		m.insertBefore("{ System.out.println($1);  }");
+		m.insertAfter("{System.out.println($2);    }");
+		cc.writeFile();
+	}
 }
--- src/test/java/com/edu/zookeeper/ZookeeperTest.java
+++ src/test/java/com/edu/zookeeper/ZookeeperTest.java
@@ -5,12 +5,22 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.nodes.PersistentEphemeralNode;
+import org.apache.curator.framework.recipes.nodes.PersistentEphemeralNode.Mode;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.test.KillSession;
+import org.apache.curator.utils.CloseableUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.edu.common.RedisConfig;
 import com.edu.zookeeper.test.Business;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,14 +28,18 @@ import com.edu.zookeeper.test.Business;
 public class ZookeeperTest {
 	@Autowired
 	private Business business;
+	//@Autowired
+	//private RedisConfig redisConfig;
 
 	private static final int QTY = 100;
 	private static final int REPETITIONS = QTY * 10;
 	private static final String PATH = "/examples/locks";
+	
 
 	/**
 	 * error the @annotation pointcut expression is only supported at Java 5
-	 * compliance level or above 兼容性，这个问题如何处理了？！
+	 * compliance level or above 兼容性，这个问题如何处理了？！ 实现了一套分布式的锁 spring +
+	 * CuratorFramework 实现一个分布式锁， 性能在50ms
 	 * 
 	 * @throws Exception
 	 */
@@ -48,6 +62,54 @@ public class ZookeeperTest {
 			service.awaitTermination(10, TimeUnit.MINUTES);
 		} finally {
 
+		}
+	}
+
+	private static final String PATH1 = "/example/ephemeralNode";
+	private static final String PATH2 = "/example/node";
+	// 定义远程连接地址
+	private static final String server = ",192.168.60.81:2181";
+	
+	/**
+	 * 临时节点测试
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_PersistentEphemeralNode() throws Exception {
+		CuratorFramework client = null;
+		PersistentEphemeralNode node = null;
+		try {
+			client = CuratorFrameworkFactory.newClient(server, new ExponentialBackoffRetry(1000, 3));
+			client.getConnectionStateListenable().addListener(new ConnectionStateListener() {
+
+				@Override
+				public void stateChanged(CuratorFramework client, ConnectionState newState) {
+					System.out.println("client 的连接状态e:" + newState.name());
+
+				}
+			});
+			client.start();
+
+			// http://zookeeper.apache.org/doc/r3.2.2/api/org/apache/zookeeper/CreateMode.html
+			node = new PersistentEphemeralNode(client, Mode.EPHEMERAL, PATH1, "test".getBytes());
+			node.start();
+			node.waitForInitialCreate(3, TimeUnit.SECONDS);
+			String actualPath = node.getActualPath();
+			System.out.println("node " + actualPath + " value: " + new String(client.getData().forPath(actualPath)));
+
+			client.create().forPath(PATH2, "persistent node".getBytes());
+			System.out.println("node " + PATH2 + " value: " + new String(client.getData().forPath(PATH2)));
+			KillSession.kill(client.getZookeeperClient().getZooKeeper());
+			System.out.println(
+					"node " + actualPath + " doesn't exist: " + (client.checkExists().forPath(actualPath) == null));
+			System.out.println("node " + PATH2 + " value: " + new String(client.getData().forPath(PATH2)));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			CloseableUtils.closeQuietly(node);
+			CloseableUtils.closeQuietly(client);
 		}
 	}
 
